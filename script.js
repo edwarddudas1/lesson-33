bbdbea0c9174f776f5be3c0418ef8feb6565fc25
Edward Dudas <edwarddudas@gmail.com>
2024-02-13 18:25:57 +0200

    update hw

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // 1 hw
 const result = 5 + 5 + '5';
 console.log(typeof result);
-// 2 hm
+// 2 hw
 const email = 'edwarddudas@gmail.com';
 console.log(email.length);
 console.log(email.includes('@'));
-// 3 hm
+// 3 hw
 const a = 'My';
 const b = 'name';
 const c = 'is';
@@ -16,7 +16,7 @@ const b1 = 'name ';
 const c1 = 'is ';
 const fullName1 = a1 + b1 + c1 + 'Edward';
 console.log(fullName1);
-// 4 hm
+// 4 hw
 const userName = 'Едуарде';
 const payment = 500;
 const message = alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`)
